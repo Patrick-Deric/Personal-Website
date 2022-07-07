a5853e1e94ba49fca24b263dc12998d2e828feae
Patrick Deric Rabello <103678580+Patrick-Deric@users.noreply.github.com>
2022-07-07 13:18:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const barraMenu = document.querySelector('.hamb');
-const navList = document.querySelector('.nav-list');
-const links = document.querySelector('.nav-list li');
-
-barraMenu.addEventListener('click', function (){
-	this.classList.toggle('click');
-	navList.classList.toggle('open')
-})
